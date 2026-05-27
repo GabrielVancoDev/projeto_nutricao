02f6fa36677567d7f44f18a05844f0d233060e91
GabrielVancoDev <gabrielvanco111223@gmail.com>
2026-05-27 14:12:51 -0300

    Inserindo validações de campos na API de usuários, impedindo inserção de caracteres inválidos

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
+import { CreateUsuarioDto } from './dto/create-usuario.dto/create-usuario.dto';
 
 @Controller('usuarios')
 export class UsuarioController {
   constructor(private readonly service: UsuarioService) {}
 
   @Post()
-  create(@Body() body: any) {
-    return this.service.create(body);
+  create(@Body() dto: CreateUsuarioDto) {
+    return this.service.create(dto);
   }
 
   @Get()
