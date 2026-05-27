642f785118c9d45288f932c3354f10d4d99fb94c
GabrielVancoDev <gabrielvanco111223@gmail.com>
2026-05-27 11:35:44 -0300

    Criando a classe service, com os métodos para criar e buscar nutricionistas

--- src/nutricionista/nutricionista.service.ts
+++ src/nutricionista/nutricionista.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose'; // Importação do InjectModel
+import { Model } from 'mongoose'; // Importação do Model
+import { Nutricionista } from './nutricionista.schema'; // Importação do Nutricionista
 
 @Injectable()
-export class NutricionistaService {}
+export class NutricionistaService {
+    constructor(
+        @InjectModel(Nutricionista.name)
+        private nutricionistaModel: Model<Nutricionista>, // Modelo do Nutricionista
+    ) {}
+
+    // Método para criar um novo Nutricionista
+    async create(data: any) {
+        return this.nutricionistaModel.create(data);
+    }
+
+    // Método para buscar todos os Nutricionistas
+    async findAll() {
+        return this.nutricionistaModel.find().populate('usuarioId');
+    }
+}
