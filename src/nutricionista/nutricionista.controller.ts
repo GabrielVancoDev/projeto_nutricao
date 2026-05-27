0b8eb8b2890d0c3b277d98d4dcf00f6a594ce3a7
GabrielVancoDev <gabrielvanco111223@gmail.com>
2026-05-27 12:10:09 -0300

    Criando a classe controller

--- src/nutricionista/nutricionista.controller.ts
+++ src/nutricionista/nutricionista.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { NutricionistaService } from './nutricionista.service';
 
 @Controller('nutricionista')
-export class NutricionistaController {}
+export class NutricionistaController {
+  // Controlador para Nutricionistas
+  constructor(private readonly service: NutricionistaService) {}
+
+  // Método para criar um novo Nutricionista
+  @Post()
+  create(@Body() body: any) {
+    return this.service.create(body);
+  }
+
+  // Método para buscar todos os Nutricionistas
+  @Get()
+  findAll() {
+    return this.service.findAll();
+  }
+}
